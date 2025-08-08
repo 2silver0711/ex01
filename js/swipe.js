c89dfed08e57122c73c6513f95960c3abebb6076
2silver0711 <peu20816@gmail.com>
2025-08-08 14:05:22 +0900

    kiosk.html

--- js/swipe.js
+++ js/swipe.js
@@ -4,9 +4,9 @@ var swiper = new Swiper(".my", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-},
-pagination: {
-    el: "swiper-pagination",
-    type: "progressbar",
-},
+    },
+    pagination: {
+        el: "swiper-pagination",
+        type: "progressbar",
+    },
 });
